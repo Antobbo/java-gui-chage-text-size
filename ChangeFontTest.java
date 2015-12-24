02553cf7ce6a91b6e6740caec94a7a5554f658c3
antobbo <antobbo@gmail.com>
2015-12-24 11:50:24 +0000

    Create ChangeFontTest.java
    Added source code

---
+++ ChangeFontTest.java
@@ -0,0 +1,10 @@
+/*ChangeFontTest.java*/
+import javax.swing.JFrame;
+public class ChangeFontTest{
+	public static void main(String[] args){
+		ChangeFont changeFont = new ChangeFont();
+		changeFont.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		changeFont.setSize(600, 200);
+		changeFont.setVisible( true );
+	}
+}
